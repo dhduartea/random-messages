5ef301d24b4f06fa684777cfa36eea46943adf96
David Duarte <dhduartea@gmail.com>
2022-07-12 11:39:55 -0500

    Random name generator

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const messages=[
+    "David",
+    "Karen",
+    "Hugo",
+    "Laura",
+    "Diana",
+    "Ana Maria",
+    "Isabela",
+    "Antonio",
+    "Norma",
+];
+const randomMsg=()=>{
+    const message=messages[Math.floor(Math.random()*messages.length)];
+    console.log(message);
+};
+
+module.exports={randomMsg};
